54201c0838cba705e301bddaebad9cea3026125f
Sakurai <K.sakurai_2000@outlook.com>
2022-08-05 09:37:38 +0000

    ８時間煮込んだ秘伝のタレ
    APIv2で動かないことが判明した編

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use dotenvy::dotenv;
+use std::env;
+use std::fs;
+use std::path::Path;
+use twitter_v2::query::MediaField;
+use twitter_v2::query::TweetField;
+use twitter_v2::{authorization::BearerToken, TwitterApi};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenv().ok();
+    let token = env::var("TW_BEARER_TOKEN").unwrap();
+    let auth = BearerToken::new(token);
+    let api = TwitterApi::new(auth);
+
+    let search_str = "#アークナイツ";
+
+    // make dir
+    let path = Path::new("./images").join(search_str);
+    println!("{:#?}", path);
+    if !path.exists() {
+        fs::create_dir_all(path).expect("create dir error");
+    }
+
+    // search
+    // Twitter-v2ではminFavの指定が出来ないらしい；；
+    // https://developer.twitter.com/en/docs/twitter-api/tweets/search/migrate
+    let query = String::from(search_str) + " has:images -is:retweet";
+    println!("query: {}", query);
+    let tweets = api
+        .get_tweets_search_recent(query)
+        .max_results(20)
+        .tweet_fields([TweetField::Entities, TweetField::Text])
+        .media_fields([MediaField::Url, MediaField::Type])
+        .send()
+        .await?;
+    let tweets = tweets.into_data();
+    println!("{:#?}", tweets);
+
+    //? Optionのネスト深すぎんか
+    // if let Some(ts) = tweets {
+    //     for t in ts {
+    //         for url in t.entities.expect("a").urls.expect("msg") {
+    //             for i in url.images.unwrap() {
+    //                 println!("{}", i.url);
+    //             }
+    //         }
+    //     }
+    // }
+
+    // download
+
+    Ok(())
 }
